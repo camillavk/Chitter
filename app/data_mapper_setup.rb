ae56c592b682c620228c5eea59ca3bbe463bd5fd
Camilla van Klinken <camillavk@gmail.com>
2014-10-12 15:02:42 +0100

    ..

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,7 @@ require './app/models/user'
 
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/Chitter_development")
+DataMapper.setup(:default, "postgres://zcebxyfqjphtcz:qJaf2sCOs87hDMCw5thNJEw2Zl@ec2-54-83-204-104.compute-1.amazonaws.com:5432/dftbg7cb6ffn39")
 
 DataMapper.finalize
 
