499a28b023d56a6d5beeb2ad81295df3d28adda2
Camilla van Klinken <camillavk@gmail.com>
2014-10-12 14:34:36 +0100

    css added

--- app/chitter.rb
+++ app/chitter.rb
@@ -12,7 +12,8 @@ require_relative 'controllers/users'
 require_relative 'controllers/sessions'
 require_relative 'controllers/messages'
 
-
+set :public_dir, Proc.new { File.join(root, "public")}
+set :public_folder, 'public'
 enable :sessions
 set :session_secret, "secret"
 use Rack::Flash
