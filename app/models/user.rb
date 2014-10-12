7532cdae11cb07fceb4d5b88cea747d5cf3b201d
Camilla van Klinken <camillavk@gmail.com>
2014-10-12 14:54:06 +0100

    Refactoring and CSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User
 	include DataMapper::Resource
 
 	property :id, 							Serial
-	property :username,					String, :unique => true
-	property :name, 						String
-	property :email, 						String, :required => true, :unique => true, :message => "This email is already taken"
+	property :username,					String, :unique => true, :required => true, 
+															:messages => {:presence => "You need to enter a username!",
+															:is_unique => "That username has already been taken"}
+	property :name, 						String, :required => true, 
+															:messages => {:presence => "We won't know who you are if you don't enter a name!"}
+	property :email, 						String, :required => true, :unique => true, :format => :email_address, 
+															:messages => {:is_unique => "This email is already taken",
+															:presence => "Oops, looks like you forgot to enter your email!",
+															:format => "Doesn't look like an email address to me!"}
 	property :password_digest,	Text, :required => true
 
 	attr_reader :password
