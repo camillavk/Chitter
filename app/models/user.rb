22a847d4ed9432e806efe048249be304b7d7818e
Camilla van Klinken <camillavk@gmail.com>
2014-10-12 12:54:52 +0100

    Email must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,26 +5,26 @@ class User
 
 	property :id, 							Serial
 	property :name, 						String
-	property :email, 						String
+	property :email, 						String, :required => true, :unique => true, :message => "This email is already taken"
 	property :password_digest,	Text, :required => true
 
 	attr_reader :password
 	attr_accessor :password_confirmation
 
-	validates_confirmation_of :password
+	validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
 
 	def password=(password)
 		@password = password 
 		self.password_digest = BCrypt::Password.create(password)
 	end
 
-	# def self.authenticate(email, password)
-	# 	user = first(:email, email)
-	# 	if user && BCrypt::Password.new(user.password_digest) == password
-	# 		user
-	# 	else
-	# 		nil
-	# 	end
-	# end
+	def self.authenticate(email, password)
+		user = first(:email, email)
+		if user && BCrypt::Password.new(user.password_digest) == password
+			user
+		else
+			nil
+		end
+	end
 
 end
